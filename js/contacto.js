7f4296c94ab5bbfc34f489ac7e459b503288a4d7
aguchumilla <agustin.chumilla@gmail.com>
2024-08-19 16:16:55 -0300

    comiit 4

--- js/contacto.js
+++ js/contacto.js
@@ -2,6 +2,7 @@
     const enviarButton = document.querySelector('.contacto_boton');
     let acept = document.querySelector('.consulta_aceptada');
     let rejeated = document.querySelector('.consulta_rechazada');
+    // const form = document.querySelector('.contacto_form');
 
     enviarButton.addEventListener('click', (event) => {
         // Prevenir que el formulario se envíe y la página se recargue
@@ -12,12 +13,14 @@
         const eml = document.querySelector("[name=email]").value;
         const com = document.querySelector("[name=comentario]").value;
 
-        if (nom.length === 0 || eml.length === 0 || com.length === 0) {
+        emailRegex = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+        if (nom.length === 0 || eml.length === 0 || com.length === 0 || !emailRegex.test(eml)) {
             rejeated.style.opacity = 1;
             acept.style.opacity = 0;
         } else {
             rejeated.style.opacity = 0;
             acept.style.opacity = 1;
+            // form.submit();
         }
     });
 
